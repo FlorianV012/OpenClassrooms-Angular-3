d19b598a577862bd32a79b728afe4a36326129ae
Florian <fvilmot@gmail.com>
2024-03-20 17:49:11 +0100

    ch1.3 post resolver creation

---
+++ src/app/social-media/resolvers/posts.resolver.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  Resolve,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { Post } from '../models/post.model';
+import { PostsService } from '../services/post.service';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class PostResolver implements Resolve<Post[]> {
+  constructor(private postsService: PostsService) {}
+
+  resolve(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<Post[]> {
+    return this.postsService.getPosts();
+  }
+}
--- src/app/social-media/social-media.module.ts
+++ src/app/social-media/social-media.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SocialMediaRoutingModule } from './social-media-routing.module';
+import { PostsService } from './services/post.service';
+import { PostResolver } from './resolvers/posts.resolver';
+import { PostListComponent } from './components/post-list/post-list.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [PostListComponent],
   imports: [CommonModule, SocialMediaRoutingModule],
+  providers: [PostsService, PostResolver],
 })
 export class SocialMediaModule {}
