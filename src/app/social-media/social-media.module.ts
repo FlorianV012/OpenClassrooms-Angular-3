4ff7e2ec771acba936e51a29d99679af9e1bc7c0
Florian <fvilmot@gmail.com>
2024-03-21 09:57:44 +0100

    ch1.4 adding comments components

--- src/app/social-media/social-media.module.ts
+++ src/app/social-media/social-media.module.ts
@@ -6,10 +6,11 @@ import { PostsService } from './services/post.service';
 import { PostResolver } from './resolvers/posts.resolver';
 import { PostListComponent } from './components/post-list/post-list.component';
 import { PostListItemComponent } from './components/post-list-item/post-list-item.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [PostListComponent, PostListItemComponent],
-  imports: [CommonModule, SocialMediaRoutingModule],
+  imports: [CommonModule, SocialMediaRoutingModule, SharedModule],
   providers: [PostsService, PostResolver],
 })
 export class SocialMediaModule {}
