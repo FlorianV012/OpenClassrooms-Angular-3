d4f88ae432218f4754fa0832886c7350f9215f34
Florian <fvilmot@gmail.com>
2024-03-21 14:37:26 +0100

    ch1.4 adding new comment

--- src/app/social-media/components/post-list/post-list.component.ts
+++ src/app/social-media/components/post-list/post-list.component.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import { Post } from '../../models/post.model';
 import { ActivatedRoute } from '@angular/router';
 import { map } from 'rxjs/operators';
+import { PostsService } from '../../services/post.service';
 
 @Component({
   selector: 'app-post-list',
@@ -12,9 +13,16 @@ import { map } from 'rxjs/operators';
 export class PostListComponent implements OnInit {
   posts$!: Observable<Post[]>;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private postsService: PostsService
+  ) {}
 
   ngOnInit(): void {
     this.posts$ = this.route.data.pipe(map((data) => data['posts']));
+  }
+
+  onPostCommented(postCommented: { comment: string; postId: number }) {
+    this.postsService.addNewComment(postCommented);
   }
 }
