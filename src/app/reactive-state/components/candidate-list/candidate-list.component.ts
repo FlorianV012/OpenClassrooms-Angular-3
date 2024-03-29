0fce5e24a4e7ab94e623f3037164e76bd887f05a
fvilmot <fvilmot@gmail.com>
2024-03-29 14:59:51 +0100

    ch4.1

---
+++ src/app/reactive-state/components/candidate-list/candidate-list.component.ts
@@ -0,0 +1,24 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnInit,
+} from '@angular/core';
+import { Observable } from 'rxjs';
+import { CandidatesService } from '../../services/candidates.service';
+
+@Component({
+  selector: 'app-candidate-list',
+  templateUrl: './candidate-list.component.html',
+  styleUrls: ['./candidate-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CandidateListComponent implements OnInit {
+  loading$!: Observable<boolean>;
+
+  constructor(private candidatesServices: CandidatesService) {}
+
+  ngOnInit(): void {
+    this.loading$ = this.candidatesServices.loading$;
+  }
+}
